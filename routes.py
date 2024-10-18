03abfb99a19abba3bae0a7cfd8fdd4ad96d18c30
Maksym Diachok <maksdyachok2005@gmail.com>
2024-10-18 23:16:47 +0300

    Remove test api. Add rout for getting user

--- routes.py
+++ routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
 
 api = Blueprint("routes", __name__)
 
-@api.route("/test", methods=["GET"])
-def get_items():
-    return jsonify([{"status": "working"}])
+@api.route("/user/<user_id>", methods=["GET"])
+def get_user(user_id):
+    file = open("db/users.csv", "r")
+    lines = file.readlines()
+
+    for line in lines:
+        [ id, name ] = line.split(",")
+        if id != user_id: continue
+
+        return jsonify({"id": id, "name": name.rstrip()})
+    
+    abort(404)
+
