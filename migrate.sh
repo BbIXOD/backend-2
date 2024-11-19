cfd1960c5486d0ee728a1064503037ff0af53626
Maksym Diachok <maksdyachok2005@gmail.com>
2024-11-20 00:42:17 +0200

    Return to old migrate script

--- migrate.sh
+++ migrate.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-until flask db migrate || flask db upgrade; do
+until flask db migrate && flask db upgrade; do
   >&2 echo "Postgres is unavailable or migration failed - sleeping"
   sleep 1
 done
